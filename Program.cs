29c9cd156ed00a6128c0798221d00bae3b0575de
Viktor <v.kurochkin89@yandex.ru>
2024-02-12 15:39:52 +0300

    Добавил код программы, являющейся решением задания

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите строки через пробел:");
+string input = Console.ReadLine();
+string[] sourceArray = input.Split(' ');
+
+int count = 0;
+for (int i = 0; i < sourceArray.Length; i++)
+{
+    if (sourceArray[i].Length <= 3)
+    {
+        count++;
+    }
+}
+
+string[] resultArray = new string[count];
+
+int index = 0;
+for (int i = 0; i < sourceArray.Length; i++)
+{
+    if (sourceArray[i].Length <= 3)
+    {
+        resultArray[index] = sourceArray[i];
+        index++;
+    }
+}
+
+Console.WriteLine("Массив строк меньше трёх символов:");
+Console.WriteLine("[" + string.Join(", ", resultArray) + "]");
