9ebbc54e4cee7c519b95326c1d7eef6c9adc96f4
Viktor <v.kurochkin89@yandex.ru>
2024-02-12 15:44:49 +0300

    Добавил комментарии к коду решения

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 ﻿Console.WriteLine("Введите строки через пробел:");
-string input = Console.ReadLine();
-string[] sourceArray = input.Split(' ');
+string input = Console.ReadLine(); // Ввод строк
+string[] sourceArray = input.Split(' '); // Преобразование в массив с разделителем пробел
 
+// Считаем количество строк с символами меньше трёх
 int count = 0;
 for (int i = 0; i < sourceArray.Length; i++)
 {
@@ -11,8 +12,10 @@ for (int i = 0; i < sourceArray.Length; i++)
     }
 }
 
+// Создаём новый массив из такого количества строк, которое подсчитано ранее
 string[] resultArray = new string[count];
 
+// Добавляем подходящие строки в созданный массив
 int index = 0;
 for (int i = 0; i < sourceArray.Length; i++)
 {
@@ -23,5 +26,6 @@ for (int i = 0; i < sourceArray.Length; i++)
     }
 }
 
+// Выводим массив в нужном формате
 Console.WriteLine("Массив строк меньше трёх символов:");
 Console.WriteLine("[" + string.Join(", ", resultArray) + "]");
